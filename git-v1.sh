127eb92fa8a4b96c97655f3bb98a4af473b4655b
Erik Dubois <erik.dubois@gmail.com>
2022-02-24 15:05:35 +0100

    push to all personal githubs

--- git-v1.sh
+++ git-v1.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
-#
+#set -e
 ##################################################################################################################
-# Written to be used on 64 bits computers
-# Author 	: 	Erik Dubois
-# Website 	: 	http://www.erikdubois.be
-##################################################################################################################
+# Author 	: Erik Dubois
+# Website   : https://www.erikdubois.be
+# Website	: https://www.arcolinux.info
+# Website	: https://www.arcolinux.com
+# Website	: https://www.arcolinuxd.com
+# Website	: https://www.arcolinuxb.com
+# Website	: https://www.arcolinuxiso.com
+# Website	: https://www.arcolinuxforum.com
 ##################################################################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
 
-
 # checking if I have the latest files from github
 echo "Checking for newer files online first"
-# Below command will backup everything inside the project folder
+git pull
 
+# Below command will backup everything inside the project folder
 git add --all .
 
 # Give a comment to the commit if you want
@@ -26,13 +30,20 @@ echo "####################################"
 read input
 
 # Committing to the local repository with a message containing the time details and commit text
-curtime=$(date)
-git commit -m "Comment : $input on $curtime"
+
+git commit -m "$input"
 
 # Push the local files to github
 
-git push -u origin master
+if grep -q main .git/config; then
+	echo "Using main"
+		git push -u origin main
+fi
 
+if grep -q master .git/config; then
+	echo "Using master"
+		git push -u origin master
+fi
 
 echo "################################################################"
 echo "###################    Git Push Done      ######################"
